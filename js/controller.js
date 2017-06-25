c7cb6cd78f2dc8e08fb7cb9188257a0d87e64348
gabrielUFCG <gabriel.coutinho@ccc.ufcg.edu.br>
2017-06-25 19:55:14 -0300

    AAAA

--- js/controller.js
+++ js/controller.js
@@ -4,6 +4,8 @@ app.controller("controller", function($scope, $http) {
     $scope.seriesWatchlist = [];
     $scope.tempSeries = [];
     $scope.existeSerie;
+    $scope.caracteristicas;
+    $scope.requisicao;
 
     $scope.pesquisarSerie = function(serie) {
 
@@ -11,18 +13,16 @@ app.controller("controller", function($scope, $http) {
             $scope.existeSerie = false;
        
             function successCallback(response) {
-                console.log($scope.seriesPerfil);
             
                 if(response.data.Response === "False") {
                     alert("Série não encontrada!");
                 } else {
                     $scope.tempSeries = response.data.Search;
-                    console.log($scope.tempSeries);
                     $scope.existeSerie = true;
                 }
             }
             function errorCallback(error) {
-                alert("erro");
+                alert(error);
             }
 
     }
@@ -42,5 +42,46 @@ app.controller("controller", function($scope, $http) {
             alert("A série já está no perfil.");
         }
     }
-    
+
+    $scope.imprimeCaracteristicas = function(serie) {
+        $scope.caracteristicas = "";
+        $scope.pegaCaracteristicas(serie);
+        alert($scope.caracteristicas);
+    }
+
+    $scope.pegaCaracteristicas = function(serie) {
+
+        console.log($scope.fazRequisicao(serie));
+        $scope.caracteristicas += "Título: " + " ";
+        $scope.caracteristicas += $scope.fazRequisicao(serie).Title + "\n";
+        $scope.caracteristicas += "Ano: " + " ";
+        $scope.caracteristicas += serie.Year + "\n";
+        $scope.caracteristicas += "Classificação etária: " + " ";
+        
+        $scope.caracteristicas += "Sinopse: " + " ";
+        
+    }
+
+    $scope.fazRequisicao = function(serie) {
+        $http.get("http://www.omdbapi.com/?i="+serie.imdbID+"&type=series&apikey=93330d3c").then(successCallback, errorCallback);
+            function successCallback(response) {
+                console.log(response.data);
+                return response.data;
+            }
+            function errorCallback(error) {
+                alert(error);
+            }
+    }
+
+    $scope.removePerfil = function(serie) {
+        var resposta=confirm("Deseja realmente excluir a série?");
+        if (resposta==true) {
+            $scope.removeSeriePerfil(serie);
+        }
+    }
+
+    $scope.removeSeriePerfil = function(serie) {
+        var posicaoSerie = $scope.seriesPerfil.indexOf(serie);
+        $scope.seriesPerfil.splice(posicaoSerie, 1);
+    }
     })
