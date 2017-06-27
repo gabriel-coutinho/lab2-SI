7435d0e85bbd7fa1afe908da730d49a436d5abc8
gabrielUFCG <gabriel.coutinho@ccc.ufcg.edu.br>
2017-06-26 23:09:36 -0300

    Acabado

--- js/controller.js
+++ js/controller.js
@@ -28,57 +28,29 @@ app.controller("controller", function($scope, $http) {
     }
 
     $scope.adcPerfil = function(serie) {
-        $scope.contem = false;
-        $scope.i = 0;
-        for (var index = 0; index < $scope.seriesPerfil.length; index++) {
-            var element = $scope.seriesPerfil[index];
-            if(element.imdbID === serie.imdbID) {
-                $scope.contem = true;
-            } 
-        }
-        if(!$scope.contem) {
-             $http.get("http://www.omdbapi.com/?i="+serie.imdbID+"&plot=full&apikey=93330d3c").then(successCallback, errorCallback);
+        if($scope.contem(serie, $scope.seriesPerfil)) {
+            alert("A série já está no perfil.");
+        } else if($scope.contem(serie, $scope.seriesWatchlist)){
+            $scope.removeWatchlist(serie);
+            $http.get("http://www.omdbapi.com/?i="+serie.imdbID+"&plot=full&apikey=93330d3c").then(successCallback, errorCallback);
             function successCallback(response) {
                  $scope.seriesPerfil.push(response.data);
             }
             function errorCallback(error) {
                 alert(error);
-        }} else{
-            alert("A série já está no perfil.");
-        }
-    }
-
-    $scope.imprimeCaracteristicas = function(serie) {
-        $scope.caracteristicas = "";
-        $scope.pegaCaracteristicas($scope.achaSeriePerfil(serie));
-        alert($scope.caracteristicas);
-    }
-
-    $scope.pegaCaracteristicas = function(serie) {
-
-        console.log($scope.fazRequisicao(serie));
-        $scope.caracteristicas += "Título: " + " ";
-        $scope.caracteristicas += serie.Title + "\n";
-        $scope.caracteristicas += "Ano: " + " ";
-        $scope.caracteristicas += serie.Year + "\n";
-        $scope.caracteristicas += "Classificação etária: " + " ";
-        $scope.caracteristicas += serie.Rated + "\n";
-        $scope.caracteristicas += "Média: " + " ";
-        $scope.caracteristicas += serie.imdbRating + "\n";
-        $scope.caracteristicas += "Sinopse: " + " ";
-        $scope.caracteristicas += serie.Plot;
-
-        
-    }
-
-    $scope.fazRequisicao = function(serie) {
-        $http.get("http://www.omdbapi.com/?i="+serie.imdbID+"&plot=full&apikey=93330d3c").then(successCallback, errorCallback);
+            }
+        } else {
+            $http.get("http://www.omdbapi.com/?i="+serie.imdbID+"&plot=full&apikey=93330d3c").then(successCallback, errorCallback);
             function successCallback(response) {
-                return response.data;
+                console.log(response.data);
+                response.data.notaPessoa = "-";
+                response.data.ultimoEp = "-";
+                 $scope.seriesPerfil.push(response.data);
             }
             function errorCallback(error) {
                 alert(error);
             }
+        }
     }
 
     $scope.removePerfil = function(serie) {
@@ -102,4 +74,57 @@ app.controller("controller", function($scope, $http) {
             
         }
     }
-    })
+    $scope.adcWatchlist = function(serie) {
+        if($scope.contem(serie,$scope.seriesPerfil)) {
+            alert("A série já está no perfil.");
+        } else if($scope.contem(serie, $scope.seriesWatchlist)) {
+            alert("A série já está na watchlist.");
+        } else {
+            $scope.seriesWatchlist.push(serie);
+        }
+    }
+
+    $scope.contem = function(serie, lista) {
+        var contem = false;
+        for (var index = 0; index < lista.length; index++) {
+            var element = lista[index];
+            if(element.imdbID === serie.imdbID) {
+                var contem = true;
+            } 
+        }
+        return contem;
+    }
+    
+    $scope.removeWatchlist = function(serie) {
+        var posicaoSerie = $scope.seriesWatchlist.indexOf(serie);
+        $scope.seriesWatchlist.splice(posicaoSerie, 1);
+    }
+
+    $scope.existeSeriePerfil = function() {
+        return $scope.seriesPerfil.length !== 0;
+    }
+
+    $scope.existeSerieWatchlist = function() {
+        return $scope.seriesWatchlist.length !== 0;
+    }
+
+     $scope.mudaEpisodio = function(serie, episodio) {
+      for (var index = 0; index < $scope.seriesPerfil.length; index++) {    
+            if ($scope.seriesPerfil[index].imdbID === serie.imdbID) {
+                $scope.seriesPerfil[index].ultimoEp = episodio;
+            }
+            
+        }
+    }
+
+     $scope.mudaNota = function(serie, nota) {
+      for (var index = 0; index < $scope.seriesPerfil.length; index++) {    
+            if ($scope.seriesPerfil[index].imdbID === serie.imdbID) {
+                $scope.seriesPerfil[index].notaPessoa = nota;
+            }
+            
+        }
+    }
+
+    
+})
